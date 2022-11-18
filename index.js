8f4dd0f4d31e170d7cf7f1bc66b3cb27631a195f
ayub2022 <ayubmaina2017@gmail.com>
2022-11-18 14:08:56 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+function writeCards(array,event){
+    var newArray = [];
+    let n =array.length;
+    for(let i=0;i<n;i++){
+        newArray.push('Thank you,'+" "+array[i]+","+" "+'for the wonderful'+" "+'surprise'+" "+'gift!')
+    }
+    return newArray
+}
+
+
+
+  function countDown (number){
+    
+        for(let i = number; !(i<0); i--){
+            console.log(i)
+        }
+}
+
+
+countDown(5)
